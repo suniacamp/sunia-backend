7839381db828868da0b70fca503fa69eedc0ca26
Michael DeMarco <michaelfromyeg@gmail.com>
2020-04-20 18:13:01 -0600

    Promo points function written, not fully tested

--- src/hello.ts
+++
@@ -1,8 +0,0 @@
-const greeter = (person: string) => {
-    return `Hello, ${person}!`;
-  }
-  
-  function testGreeter() {
-    const user = 'Grant';
-    Logger.log(greeter(user));
-  }
---
+++ src/promoPoints.ts
@@ -0,0 +1,57 @@
+// IF YOU ARE READING HTIS IN ON THE WEB EDITOR, DO NOT EDIT DIRECTLY, EDIT THE TYPESCRIPT SOURCE
+// visit https://github.com/suniacamp to see the repositories
+
+// Provides Slack an update with current promo point standings
+function promoPoints() {
+    Logger.log("Sending promo point update...");
+
+    const promoPointsSource = PropertiesService.getScriptProperties().getProperty("promoPointsSource");
+    const promoPointsSheet = SpreadsheetApp.openById(promoPointsSource);
+    const totalSheet = promoPointsSheet.getSheetByName("TOTALS");
+
+    const totalStaff: number = totalSheet.getLastRow();
+
+    let xOffset = 2;
+    let yOffset = 3;
+
+    let message = "Promo Point Standings for the Week of " + new Date() + "\n";
+
+    for (let i = yOffset; i < totalStaff; i++) {
+        let name = totalSheet.getRange(i, xOffset).getValue();
+        let points = totalSheet.getRange(i, xOffset + 1).getValue();
+        message += name + ": " + points + "\n";
+    }
+    
+    Logger.log("Done!");
+}
+
+// Send the point message to Slack by grabbing web hook URL and creating payloads
+function sendPointsMessage(message: string) {
+
+    let slackWebhook = PropertiesService.getScriptProperties().getProperty("slackTestingWebhook");
+    
+    let testingPayload = {
+        "channel" : "#testing", 
+        "text" : message
+    }
+
+    let promoPayload = {
+    "channel" : "#promo",
+    "text" : message
+    }
+    
+    sendToSlack(slackWebhook, testingPayload);
+    // sendToSlack(slackWebhook, promoPayload);
+
+}
+
+// Slack boilerplate
+function sendToSlack (url: string, payload: string) {
+    var options: Object =  {
+      "method" : "post",
+      "contentType" : "application/json",
+      "payload" : JSON.stringify(payload)
+    };
+
+    return UrlFetchApp.fetch(url, options)
+  }
